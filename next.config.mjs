b2246b1890fa06b1026a4e80b880765754e2309e
Xinjiang Shao <shaoxinjiang@gmail.com>
2024-07-09 12:58:25 -0500

    feat: add speed insights

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
+    experimental: {
+        reactCompiler: true,
+    },
+};
 
 export default nextConfig;
