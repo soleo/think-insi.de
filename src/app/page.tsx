4eb8ccb219d4763b7819d4f289dbb0c385525e88
Xinjiang Shao <shaoxinjiang@gmail.com>
2024-07-22 23:51:59 -0500

    fix: fix priority attribute for images

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
       <main>
         <Stack spacing={2} padding={4} flex={1} alignContent={"center"} alignItems={"center"} paddingTop={10}>
           <Typography variant="h1" textAlign="center">My Life Tracker</Typography>
-          <Image src="/illustration.webp" alt="Think Inside Illustration" width={1200} height={686} priority="high" />
+          <Image src="/illustration.webp" alt="Think Inside Illustration" width={1200} height={686} priority={true} />
 
           <Typography variant="h2" textAlign="center">
             Where Have I Been? Where Am I Going?
