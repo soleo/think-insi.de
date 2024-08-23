b7f10a17b657a838ff0ad0c067a8ffd95c30658d
Xinjiang Shao <shaoxinjiang@gmail.com>
2024-08-23 15:18:24 -0500

    feat: add custom otel configuration

---
+++ src/instrumentation.node.ts
@@ -0,0 +1,22 @@
+import { NodeSDK } from '@opentelemetry/sdk-node';
+import { getNodeAutoInstrumentations } from '@opentelemetry/auto-instrumentations-node';
+import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-grpc';
+import { OTLPMetricExporter } from '@opentelemetry/exporter-metrics-otlp-grpc';
+import { PeriodicExportingMetricReader } from '@opentelemetry/sdk-metrics';
+
+const sdk = new NodeSDK({
+  traceExporter: new OTLPTraceExporter(),
+  instrumentations: [
+    getNodeAutoInstrumentations({
+      // disable fs instrumentation to reduce noise
+      '@opentelemetry/instrumentation-fs': {
+        enabled: false,
+      },
+    })
+  ],
+  metricReader: new PeriodicExportingMetricReader({
+    exporter: new OTLPMetricExporter(),
+  }),
+});
+
+sdk.start();
--- src/instrumentation.ts
+++ src/instrumentation.ts
@@ -1,18 +1,5 @@
-import { registerOTel } from '@vercel/otel'
-import { SamplingDecision } from '@opentelemetry/sdk-trace-base'
-import { Context } from '@opentelemetry/api'
-
-export function register() {
-  registerOTel({
-    serviceName: 'think-inside-app',
-    traceExporter: 'auto',
-    spanProcessors: ['auto'],
-    traceSampler: {
-      shouldSample: (context: Context) => {
-        return {
-          decision: SamplingDecision.RECORD_AND_SAMPLED
-        }
-      },
-    },
-  })
+export async function register() {
+  if (process.env.NEXT_RUNTIME === 'nodejs') {
+    await import('./instrumentation.node')
+  }
 }
